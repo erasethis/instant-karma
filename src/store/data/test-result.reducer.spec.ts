41931033643aeb7a4591373ae8a02bda35c0382f
Thorsten Westheider <Thorsten Westheider>
2017-02-04 12:32:09 +0100

    Design changes

--- src/store/data/test-result.reducer.spec.ts
+++ src/store/data/test-result.reducer.spec.ts
@@ -12,26 +12,10 @@ import { testResult, ITestResultState, TEST_RESULT_INIT_STATE } from './test-res
 describe('test result reducer', () => {
     describe('initial state', () => {
         it('should have its "success" property set to "false"', () => {
-            expect(TEST_RESULT_INIT_STATE.get('success')).toBeFalse();
+            expect(TEST_RESULT_INIT_STATE.get('success')).toBeTrue();
         });
         it('should have its "log" property set to an empty array', () => {
             expect(TEST_RESULT_INIT_STATE.get('log').toJS()).toBeEmptyArray();
-        });
-    });
-    describe('when test run is starting', () => {
-        it('should set its "busy" property to "true"', () => {
-            expect(progress(undefined, {
-                type: KARMA_ACTIONS.KARMA_BROWSER_START,
-                payload: { }
-            }).get('busy')).toBeTrue();
-        });
-    });
-    describe('when test run is completed', () => {
-         it('should set its "busy" property to "false"', () => {
-         expect(progress(undefined, {
-                type: KARMA_ACTIONS.KARMA_RUN_COMPLETE,
-                payload: { }
-            }).get('busy')).toBeFalse();
         });
     });
 });
